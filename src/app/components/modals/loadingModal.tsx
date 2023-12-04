9aa3ee933b410d3cc5be07be9ff24e68caf474d1
kayiganwa04 <kayiganwa04@gmail.com>
2023-12-04 12:55:28 +0200

    added loader changes

--- src/app/components/modals/loadingModal.tsx
+++ src/app/components/modals/loadingModal.tsx
@@ -8,7 +8,7 @@ export default function LoadingModal({
   isOpen: boolean;
 }) {
   return (
-    <Modal isOpen={true}>
+    <Modal isOpen={isOpen}>
       <div className="flex items-center justify-center w-full h-full fixed top-0 left-0 z-50">
         <div className="flex justify-center items-center">
           <div className="w-20 h-20 rounded-full animate-spin
