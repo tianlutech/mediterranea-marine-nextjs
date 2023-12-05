4e11a6a2fb56b0447a765ce28c16f8e57568fe4b
Abel Bordonado <abelbordonado@gmail.com>
2023-12-05 21:30:39 +0700

    Fix

--- src/app/components/modals/prepaymentModal.tsx
+++ src/app/components/modals/prepaymentModal.tsx
@@ -1,33 +1,42 @@
 "use client";
 import Image from "next/image";
 import Boat from "@/app/assets/boat.png";
-import Modal from "@/app/components/common/modal/modal"
-
-
-interface Mile {
-  name: string;
-  value: number
-}
+import Modal from "@/app/components/common/modal/modal";
 
 export default function PrepaymentModal({
   isOpen,
   closeModal,
-  data
+  data,
 }: {
   isOpen: boolean;
   closeModal: any;
-  data: any
+  data: Array<{ value: string; name: string }>;
 }) {
   return (
     <Modal isOpen={isOpen}>
       <div className="relative p-2 md:w-[60%] w-[95%] bg-white rounded-lg shadow">
         <div className="flex items-center justify-between px-4 pt-4 md:px-4 md:pt-4 ">
-          <h3 className="text-xl font-semibold text-black">
-            Prepay your  fuel
-          </h3>
-          <button onClick={() => closeModal()} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
-            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+          <h3 className="text-xl font-semibold text-black">Prepay your fuel</h3>
+          <button
+            onClick={() => closeModal()}
+            type="button"
+            className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+            data-modal-hide="default-modal"
+          >
+            <svg
+              className="w-3 h-3"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+              />
             </svg>
           </button>
         </div>
@@ -54,30 +63,25 @@ export default function PrepaymentModal({
                   className="w-4 h-4 text-black bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                 />
                 <label className="ms-2 text-base text-black">
-                  I want to conitinue without prepayment
+                  I want to continue without prepayment
                 </label>
               </div>
-              {
-                data.map((item: Mile, index: number) => {
-                  return (
-                    <div
-                      key={index}
-                      className="flex mt-4 items-center">
-                      <input
-                        id="default-radio-2"
-                        type="radio"
-                        value={item.value}
-                        name="default-radio"
-                        className="w-4 h-4 text-black bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                      />
-                      <label className="ms-2 text-base text-black">
-                        {item.name}
-                      </label>
-                    </div>
-                  )
-
-                })
-              }
+              {data.map((item, index: number) => {
+                return (
+                  <div key={index} className="flex mt-4 items-center">
+                    <input
+                      id="default-radio-2"
+                      type="radio"
+                      value={item.value}
+                      name="default-radio"
+                      className="w-4 h-4 text-black bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    />
+                    <label className="ms-2 text-base text-black">
+                      {item.name}
+                    </label>
+                  </div>
+                );
+              })}
             </div>
           </div>
           <div className="md:w-[50%] w-full">
