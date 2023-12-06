72abd0b2e53773439d55091ac1ace91e43257fa3
Abel Bordonado <abelbordonado@gmail.com>
2023-12-06 22:19:41 +0700

    Refactor the Spinner

--- src/app/components/modals/loadingModal.tsx
+++ src/app/components/modals/loadingModal.tsx
@@ -1,24 +1,15 @@
 "use client";
-import Modal from "@/app/components/common/modal/modal"
+import Modal from "@/app/components/common/modal/modal";
 import BoatSvg from "@/app/assets/svgs/BoatSvg";
+import Spinner from "../common/buttons/spinner";
 
-export default function LoadingModal({
-  isOpen,
-}: {
-  isOpen: boolean;
-}) {
+export default function LoadingModal({ isOpen }: { isOpen: boolean }) {
   return (
     <Modal isOpen={isOpen}>
       <div className="flex items-center justify-center w-full h-full fixed top-0 left-0 z-50">
-        <div className="flex justify-center items-center">
-          <div className="w-20 h-20 rounded-full animate-spin
-              border-r-4 border-solid border-blue-400
-              shadow-md">
-          </div>
-          <div className="absolute">
-            <BoatSvg />
-          </div>
-        </div>
+        <Spinner size={20}>
+          <BoatSvg />
+        </Spinner>
       </div>
     </Modal>
   );
