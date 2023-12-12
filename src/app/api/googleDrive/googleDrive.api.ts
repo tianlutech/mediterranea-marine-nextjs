27edab3c5ff24106a37bf18492db8b36de9aa2ba
Abel Bordonado <abelbordonado@gmail.com>
2023-12-12 20:55:28 +0700

    Changed read stream from buffer

--- src/app/api/googleDrive/googleDrive.api.ts
+++ src/app/api/googleDrive/googleDrive.api.ts
@@ -1,24 +1,28 @@
-import { google, drive_v3 } from "googleapis"
-import fs from "fs"
+import { google, drive_v3 } from "googleapis";
+import fs from "fs";
+import { Readable } from "stream";
 
 // abel am not sure about the type of this auth
 export const uploadFile = async (auth: any, file: File) => {
   // allows you to use drive API methods e.g. listing files, creating files.
-  const drive = google.drive({ version: "v3", auth })
+  const drive = google.drive({ version: "v3", auth });
+
   try {
+    const buffer = Buffer.from(await file.arrayBuffer());
+
     const res = await drive.files.create({
       requestBody: {
         name: file.name,
-        mimeType: file.type
+        mimeType: file.type,
       },
       media: {
         mimeType: file.type,
-        body: fs.createReadStream(file.name),
-      }
+        body: Readable.from(buffer),
+      },
     });
     console.log(res.data);
   } catch (error: any) {
-    console.error("Error fetching files:", error.message)
-    return null
+    console.error("Error fetching files:", error.message);
+    return null;
   }
-}
+};
