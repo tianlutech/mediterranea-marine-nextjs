51f326b5a8060b97574c8f84ec6847ae4d1365b1
Abel Bordonado <abelbordonado@gmail.com>
2023-12-21 19:13:35 +0800

    Updated .env.example

--- src/app/api/googleDrive/googleDrive.api.ts
+++ src/app/api/googleDrive/googleDrive.api.ts
@@ -1,10 +1,9 @@
 import { google, drive_v3 } from "googleapis";
-import moment from "moment"
+import moment from "moment";
 import fs from "fs";
 import { Readable } from "stream";
 import { FileBody } from "@/app/models/models";
-import { GaxiosPromise } from "googleapis/build/src/apis/abusiveexperiencereport";
-import { FileMetadata,  } from "@/app/models/models";
+import { FileMetadata } from "@/app/models/models";
 
 // abel am not sure about the type of this auth
 export const uploadFile = async (auth: any, file: File, body: FileBody) => {
@@ -13,36 +12,36 @@ export const uploadFile = async (auth: any, file: File, body: FileBody) => {
   try {
     const buffer = Buffer.from(await file.arrayBuffer());
 
-    const googleDriveFolderId = process.env.GOOGLE_DRIVE_FOLDER_ID
-    const date = moment(Date.now()).format("DD-MM-YYYY")
-    const { boatName, id, slag} = body
+    const googleDriveFolderId = process.env.GOOGLE_DRIVE_FOLDER_ID;
+    const date = moment(Date.now()).format("DD-MM-YYYY");
+    const { boatName, id, slag } = body;
 
     const fileMetadata: FileMetadata = {
       name: `${boatName}/${date}/${id}_${slag}`,
       mimeType: "application/vnd.google-apps.folder",
-      parents: [`${googleDriveFolderId}`]
+      parents: [`${googleDriveFolderId}`],
     };
     const folder = await drive.files.create({
       requestBody: fileMetadata,
       fields: "id",
     });
 
-    if(!folder.data.id){
-      return
+    if (!folder.data.id) {
+      return;
     }
 
     const res = await drive.files.create({
       requestBody: {
         name: file.name,
         mimeType: file.type,
-        parents: [`${folder.data.id}`]
+        parents: [`${folder.data.id}`],
       },
       media: {
         mimeType: file.type,
         body: Readable.from(buffer),
       },
     });
-    return res.data
+    return res.data;
   } catch (error: any) {
     console.error("Error fetching files:", error.message);
     return null;
