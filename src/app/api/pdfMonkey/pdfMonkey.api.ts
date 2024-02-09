4d262251f019e660fcf86692f53d00ec0ad4d511
Abel Bordonado <abelbordonado@gmail.com>
2024-02-09 23:23:43 +0000

    Added email to metadata

--- src/app/api/pdfMonkey/pdfMonkey.api.ts
+++ src/app/api/pdfMonkey/pdfMonkey.api.ts
@@ -1,46 +1,56 @@
 import { PDFMONKEY_DOCUMENT_ID } from "@/models/constants";
 import { Booking, Boat, Captain } from "@/models/models";
 import moment from "moment";
-import { getFileContentBase64FromGoogleDrive } from "../googleDrive/googleDrive.api"
-import axios from "axios"
+import { getFileContentBase64FromGoogleDrive } from "../googleDrive/googleDrive.api";
+import axios from "axios";
 
-const pdfMonkey_api_key = process.env.PDFMONKEY_API_KEY
+const pdfMonkey_api_key = process.env.PDFMONKEY_API_KEY;
 
-export async function createDocument(bookingInfo: Booking, boatDetails: Boat, captainDetails: Captain) {
+export async function createDocument(
+  bookingInfo: Booking,
+  boatDetails: Boat,
+  captainDetails: Captain
+) {
   try {
     const convertSignature = async (signature_url: string) => {
       try {
         // Fetch the image as a response stream
         const response = await axios.get(signature_url, {
-          responseType: "arraybuffer" // Important to handle binary data correctly
+          responseType: "arraybuffer", // Important to handle binary data correctly
         });
-    
+
         // Convert the response data (buffer) to a base64 string
-        const base64String = Buffer.from(response.data, "binary").toString("base64");
-        
+        const base64String = Buffer.from(response.data, "binary").toString(
+          "base64"
+        );
+
         return `data:image/png;base64,${base64String}`;
       } catch (error) {
         console.error("Error retrieving file from URL:", error);
         throw error; // Rethrow the error for further handling
       }
     };
-    
-    const captainSignature = await convertSignature(captainDetails.Signature[0].url)
-    const customerSignature = await getFileContentBase64FromGoogleDrive(bookingInfo.CustomerSignature)
+
+    const captainSignature = await convertSignature(
+      captainDetails.Signature[0].url
+    );
+    const customerSignature = await getFileContentBase64FromGoogleDrive(
+      bookingInfo.CustomerSignature
+    );
 
     const date = bookingInfo["Date"];
     const bookingDateYear = moment(date).format("YYYY");
     const bookingDateMonth = moment(date).format("MM");
     const bookingDateDay = moment(date).format("HH");
-    const {Nombre, Code, RegistrationPlate, id} = boatDetails
-    const apiUrl = "https://api.pdfmonkey.io/api/v1/documents"
+    const { Nombre, Code, RegistrationPlate, id } = boatDetails;
+    const apiUrl = "https://api.pdfmonkey.io/api/v1/documents";
 
     const response = await fetch(apiUrl, {
       method: "POST",
       headers: {
-        "Accept": "application/json",
+        Accept: "application/json",
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${pdfMonkey_api_key}`
+        Authorization: `Bearer ${pdfMonkey_api_key}`,
       },
       body: JSON.stringify({
         document: {
@@ -50,45 +60,52 @@ export async function createDocument(bookingInfo: Booking, boatDetails: Boat, ca
             boatName: Nombre,
             boatCode: Code,
             boatRegistrationPlate: RegistrationPlate,
-            basePort:"Ibiza",
+            basePort: "Ibiza",
             boatMaxPassenger: boatDetails["Max.Passengers"],
             bookingDepartureTime: bookingInfo["Departure Time"],
             bookingDateDay: bookingDateDay,
-            bookingDateMonth:bookingDateMonth,
-            bookingDateYear:bookingDateYear,
-            customerName: `${bookingInfo["First Name"]} ` + `${bookingInfo["Last Name"]}`,
-            bookingDate: moment(bookingInfo["SubmittedFormAt"]).format("DD/MM/YYYY"),
-            registrationPort:"Ibiza 01",
+            bookingDateMonth: bookingDateMonth,
+            bookingDateYear: bookingDateYear,
+            customerName:
+              `${bookingInfo["First Name"]} ` + `${bookingInfo["Last Name"]}`,
+            bookingDate: moment(bookingInfo["SubmittedFormAt"]).format(
+              "DD/MM/YYYY"
+            ),
+            registrationPort: "Ibiza 01",
             boatFlag: "boatFlag",
-            boatType:"Yatch",
-            departureMaximumHour:"21:30",
-            PortOfDisembark:"Ibiza",
-            MaximumNumberOfGuestCruisingOnBoard:"4",
-            CrewComposedOfCaptain:"5",
-            taxableBase:"233",
-            taxVAT:"18",
-            TotalRentalRate:"43",
-            DeliveryPort:"Ibiza port",
+            boatType: "Yatch",
+            departureMaximumHour: "21:30",
+            PortOfDisembark: "Ibiza",
+            MaximumNumberOfGuestCruisingOnBoard: "4",
+            CrewComposedOfCaptain: "5",
+            taxableBase: "233",
+            taxVAT: "18",
+            TotalRentalRate: "43",
+            DeliveryPort: "Ibiza port",
             CustomerSignature: `data:image/png;base64,${customerSignature}`,
             CaptainSignature: captainSignature,
           },
           meta: {
             clientId: "ABC1234-DE",
-            clientName: `${bookingInfo["First Name"]} ` + `${bookingInfo["Last Name"]}`,
+            clientName:
+              `${bookingInfo["First Name"]} ` + `${bookingInfo["Last Name"]}`,
+            clientEmail: bookingInfo.Email,
             boatId: id,
             captainId: `${captainDetails.id}`,
-            _filename: `${bookingInfo["First Name"]} ` + `${bookingInfo["Last Name"]} - ${moment(bookingInfo.Date).format("YYYY-MM-DD")} - contract`,
+            _filename:
+              `${bookingInfo["First Name"]} ` +
+              `${bookingInfo["Last Name"]} - ${moment(bookingInfo.Date).format(
+                "YYYY-MM-DD"
+              )} - contract`,
             NotionId: bookingInfo.id,
-          }
-        }
-      })
+          },
+        },
+      }),
     });
-    
+
     const res = await response.json();
-    return res
-  } 
-  catch (error: any)
-   {
+    return res;
+  } catch (error: any) {
     console.error("Error while creating document", error.message);
     return { error: error.message };
   }
