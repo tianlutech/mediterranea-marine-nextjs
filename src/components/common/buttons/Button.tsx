c4609eef2a77b4ce43c74559b7ddca3d006843cd
kayiganwa04 <kayiganwa04@gmail.com>
2024-02-29 10:55:35 +0200

    added missing varialble to fix the build issue

--- src/components/common/buttons/Button.tsx
+++ src/components/common/buttons/Button.tsx
@@ -2,14 +2,16 @@
 export default function Button({
   onClick,
   message,
-  className
+  className,
+  disabled
 }: {
   onClick: () => void;
   message: string;
   className?: string;
+  disabled?: boolean;
 }
 ) {
   return (
-    <button onClick={() => onClick()} data-modal-hide="default-modal" type="button" className={`${className}  border border-bgColor2 font-medium rounded-lg text-sm px-5 py-2.5 text-center`}>{message}</button>
+    <button disabled={disabled} onClick={() => onClick()} data-modal-hide="default-modal" type="button" className={`${className}  border border-bgColor2 font-medium rounded-lg text-sm px-5 py-2.5 text-center`}>{message}</button>
   )
 }
