7b2a8bbe250c4cda354c25bb4ae9025951209cf5
Abel Bordonado <abelbordonado@gmail.com>
2024-06-26 22:45:36 +0800

    Enable dynamic message to whatsapp redirect

--- src/app/info/page.tsx
+++ src/app/info/page.tsx
@@ -3,17 +3,22 @@
 import BoatSvg from "@/assets/svgs/BoatSvg";
 import Spinner from "@/components/common/containers/spinner";
 import { t } from "i18next";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 import NoSSR from "react-no-ssr";
 
 export default function Info({ params }: { params: { id: string } }) {
+  const searchParams = useSearchParams();
+  searchParams
+  const defaultMessage = "Hi 👋🏼 Please send me more info about this year new fleet ⛵ "
+  const finalMessage = searchParams.get("m") || searchParams.get("message") || defaultMessage
   /**
    * Redirects to the whatsapp account of david
    */
   useEffect(() => {
     window.location.href =
-      "https://api.whatsapp.com/send/?phone=34686598418&text=Hi 👋🏼 Please send me more info about this year new fleet ⛵ ";
-  });
+      "https://api.whatsapp.com/send/?phone=34686598418&text=" + finalMessage;
+  }, [finalMessage]);
   return (
     <NoSSR>
       <div className="relative p-2 w-full h-screen bg-white text-center flex flex-col items-center text-white justify-center">
